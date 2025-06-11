294ea86a3c4e2469c07156fc0924f86f20f0c850
David Maldonado <gorossy07@gmail.com>
2025-06-11 11:59:50 -0500

    fix: variable name

--- neurons/validator.py
+++ neurons/validator.py
@@ -3,14 +3,14 @@
 # Copyright © 2023 Rapiiidooo
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 # and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of
 # the Software.
 #
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 # THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
@@ -831,9 +831,9 @@ class Validator:
             host = miner_info['host']
             bt.logging.trace(f"{hotkey}: Allocated Miner for testing.")
 
-            # Check user_port
+            # Check fixed_external_user_port
             try:
-                user_port = miner_info.get('user_port', 27015)
+                user_port = miner_info.get('fixed_external_user_port', 27015)
                 response = requests.get(f"http://{host}:{user_port}", timeout=2)
                 if response.status_code != 200:
                     bt.logging.info(f"{hotkey}: Port {user_port} HTTP server not responding correctly")
@@ -1005,7 +1005,7 @@ class Validator:
                         miner_info = {
                             'host': axon.ip,
                             'port': info['port'],
-                            'user_port': info.get('user_port'),
+                            'fixed_external_user_port': info.get('fixed_external_user_port'),
                             'username': info['username'],
                             'password': info['password'],
                         }
